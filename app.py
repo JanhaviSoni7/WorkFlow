0fbeda640733bd70dfd682e76e6ad9314dbdda93
JanhaviSoni7 <121169112+JanhaviSoni7@users.noreply.github.com>
2025-07-26 16:09:00 +0000

    added the Flask project

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask  #module, class
 
 app = Flask(__name__)  #obj of class Flask
 
+
 @app.route("/")  #path after the domain name
 def hello_world():
-  return "Hello, World!"
+  return "Hello, sir!"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+  app.run(host='0.0.0.0', debug=True)
