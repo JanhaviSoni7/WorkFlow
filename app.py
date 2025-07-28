3e1a2872d8c6ab3e057c88abe153b659c872346d
JanhaviSoni7 <121169112+JanhaviSoni7@users.noreply.github.com>
2025-07-28 15:38:29 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask  #module, class
+from flask import Flask, render_template  #module, class
 
 app = Flask(__name__)  #obj of class Flask
 
 
 @app.route("/")  #path after the domain name
 def hello_world():
-  return "Hello, sir!"
+  return render_template("home.html")
 
 
 if __name__ == "__main__":
